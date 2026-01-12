0e5d1aef9592a39280da1f7e7cdd3973bcf43931
Girish <thoratgirish286@gmail.com>
2026-01-12 22:04:47 +0530

    Done with the hoisting

---
+++ 01_Basics/03_Number.js
@@ -0,0 +1,9 @@
+const num = 100;
+console.log(num);
+
+const num1 = new Number(100);
+console.log(num1);
+
+const concatNumber = num1 + "Girish";
+console.log(concatNumber);
+console.log(typeof concatNumber);
